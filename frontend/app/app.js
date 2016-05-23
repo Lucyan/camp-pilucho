0b56088dddef24a8abc2b827fe2cbcf051f56107
Leonardo Olivares <leonardo.olivares@wikot.com>
2016-05-23 16:36:50 -0400

    UPDATE: stack inicial

---
+++ frontend/app/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var saludalosojos = angular.module("saludalosojosApp",["ngRoute","ui.bootstrap", "ngProgress"]);
+
+/**
+ *  Main App Controller
+ **/
+
+saludalosojos.controller('mainController', ['$scope', function ($scope) {
+
+}]);
---
+++ frontend/app/modules/home/homeController.js
@@ -0,0 +1,10 @@
+saludalosojos.controller("homeController", function ($scope, $rootScope) {
+
+  $scope.anglum = {
+    url   : 'http://wikot.io/anglum',
+    title : 'Salud a los Ojos'
+  };
+
+  $scope.message = 'Campanario.';
+
+});
