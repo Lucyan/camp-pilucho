fec1b460816d2030468458f848d0f17b80c942dd
Leonardo Olivares <leonardo.olivares@wikot.com>
2016-06-07 18:49:12 -0400

    UPDATE: Se agrega fecha de sorteo a sistema

--- frontend/admin/app/modules/sorteos/modal.controller.js
+++ frontend/admin/app/modules/sorteos/modal.controller.js
@@ -1,5 +1,12 @@
 piluchoAdminAPP.controller("sorteosModalController", function ($scope, $http, $uibModalInstance) {
 
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
+
+	$scope.fechaSorteo = dd + '/' + mm + '/' + yyyy;
+
 	$scope.haymas = true;
 	$scope.userList = [];
 	$scope.userListPage = 0;
