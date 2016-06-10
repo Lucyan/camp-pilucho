3f4db5d8e7d6dc86106942c14e6ce9c1db31b577
Leonardo Olivares <leonardo.olivares@wikot.com>
2016-06-10 14:41:40 -0400

    UPDATE: Se genera vista movil de ganadores

--- frontend/app/modules/ganadores/ganadores.controller.js
+++ frontend/app/modules/ganadores/ganadores.controller.js
@@ -14,4 +14,17 @@ piluchoApp.controller("ganadoresController", function ($scope, $http) {
 	$http.get('/api/winners/').then(function(resp) {
 		$scope.sorteos = resp.data.winners;
 	});
+
+	$scope.openWinner = function($event) {
+		$ele = $($event.currentTarget);
+		if ($ele.hasClass('fa-plus')) {
+			$('.packs-movil .user.open').removeClass('open');
+			$('.packs-movil .sorteo i.fa-minus').removeClass('fa-minus').addClass('fa-plus');
+			$ele.removeClass('fa-plus').addClass('fa-minus');
+			$ele.parent().parent().find('.user').addClass('open');
+		} else {
+			$ele.removeClass('fa-minus').addClass('fa-plus');
+			$ele.parent().parent().find('.user').removeClass('open');
+		}
+	}
 });
